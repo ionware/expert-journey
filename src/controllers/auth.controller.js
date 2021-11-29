593d85c8dab4f17b5048a3164a79cc7a918cbf6d
ionware <ionwarez@gmail.com>
2021-11-29 20:09:43 +0100

    implement sign in endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,13 +2,33 @@ const registerValidator = require('@requests/validators/user');
 const Hash = require('@utils/hash');
 const JWT = require('@utils/jwt');
 const routeWrapper = require('@utils/async');
-const { createUser } = require('@services/user.service');
+const { createUser, getUserByEmail } = require('@services/user.service');
 
 /**
  * Create a new login session
  */
-const create = routeWrapper((req, res) => {
-  // Needed: Hash compare, JWT, User
+const create = routeWrapper(async (req, res) => {
+  const { email, password } = req.body;
+  const errorResponse = {
+    errors: [{ details: 'Email or password was incorrect' }],
+    message: 'Email or password is incorrect',
+  };
+
+  if (!email || !password) return res.status(400).json(errorResponse);
+
+  // fetch the user from database
+  const user = await getUserByEmail(email);
+
+  if (!user) return res.status(400).json(errorResponse);
+  // verify the password is correct
+  if (!Hash.check(password, user.password))
+    return res.status(400).json(errorResponse);
+
+  return res.status(200).json({
+    token: JWT.sign({ id: user.id, email: user.email, name: user.name }),
+    email: user.email,
+    name: user.name,
+  });
 });
 
 /**
